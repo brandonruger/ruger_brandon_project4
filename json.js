4effd0d2ce5398feabad90e60281bfae2c93fba2
r02548 <bruger@fullsail.edu>
2013-08-26 21:07:54 -0400

    Set up file for Json data

---
+++ json.js
@@ -0,0 +1,26 @@
+var json = {
+    "reminder1": {
+        "petname": ["Pet Name:", "Ryder"],
+        "petage": ["Pet Age:", "2"],
+        "pettype": ["Pet Type:", "dog"],
+        "flea": ["Flea:", "Flea Medication"],
+        "fleaRx": ["Flea Rx:", "Topical"],
+        "heartworm": ["Heartworm:", "No"],
+        "other": ["Other:", "No"],
+        "date": ["Date:", "9/20/2013"],
+        "range": ["Range:", "3"],
+        "note": ["Note:", "Reminder #1"]
+    },
+    "reminder2": {
+        "petname": ["Pet Name:", "Riley"],
+        "petage": ["Pet Age:", "1"],
+        "pettype": ["Pet Type:", "dog"],
+        "flea": ["Flea:", "No"],
+        "fleaRx": ["Flea Rx:", "--Type of Flea Medication--"],
+        "heartworm": ["Heartworm:", "Heartworm Medication"],
+        "other": ["Other:", "No"],
+        "date": ["Date:", "9/15/2013"],
+        "range": ["Range:", "3"],
+        "note": ["Note:", "Reminder #2"]
+       
+}
